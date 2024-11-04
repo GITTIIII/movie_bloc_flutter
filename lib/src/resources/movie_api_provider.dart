7e963d62d52145cc011a6e112a72fca7bf936862
GITTIIII <panupongsrithai445@gmail.com>
2024-11-04 11:25:46 +0700

    done

--- lib/src/resources/movie_api_provider.dart
+++ lib/src/resources/movie_api_provider.dart
@@ -2,15 +2,16 @@ import 'dart:async';
 import 'package:http/http.dart' show Client;
 import 'dart:convert';
 import '../models/item_model.dart';
+import '../models/trailer_model.dart';
 
 class MovieApiProvider {
   Client client = Client();
+  final _baseUrl = "http://api.themoviedb.org/3/movie";
   final _apiKey = 'ed514d0332722005ad4ffcbd5f2dcf9f';
 
   Future<ItemModel> fetchMovieList() async {
     print("entered");
-    Uri url =
-        Uri.parse("http://api.themoviedb.org/3/movie/popular?api_key=$_apiKey");
+    Uri url = Uri.parse("$_baseUrl/popular?api_key=$_apiKey");
     final response = await client.get(url);
     print(response.body.toString());
     if (response.statusCode == 200) {
@@ -19,6 +20,17 @@ class MovieApiProvider {
     } else {
       // If that call was not successful, throw an error.
       throw Exception('Failed to load post');
+    }
+  }
+
+  Future<TrailerModel> fetchTrailer(int movieId) async {
+    Uri url = Uri.parse("$_baseUrl/$movieId/videos?api_key=$_apiKey");
+    final response = await client.get(url);
+
+    if (response.statusCode == 200) {
+      return TrailerModel.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load trailers');
     }
   }
 }
